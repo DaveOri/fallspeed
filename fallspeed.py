6a99252426d6f62b823a856ac507b3c486abae61
Davide Ori <davide.ori87@gmail.com>
2018-05-18 19:53:06 +0200

    first implementation working

--- fallspeed.py
+++ fallspeed.py
@@ -16,11 +16,23 @@ metadata='subsequent-0.1/dendrite-650e-6-720-0.1-subsequent-7cVE9G0d.agg.gz.meta
 
 shape=np.loadtxt(shapefile)
 attributes=eval(open(metadata).read())
+d=attributes['grid_res']
+hull3d=ConvexHull(shape)
+hull3d=hull3d.points[hull3d.vertices]
+
+dmax = 0
+for i in range(1,hull3d.shape[0]):
+    p1 = hull3d[i,:]
+    p0 = hull3d[i-1,:]
+    dist = np.dot(p0,p1)
+    if dist > d:
+        dmax = dist
+dmax = d*dmax**0.5
 
 xy=(0,1)
 yz=(1,2)
 xz=(0,2)
-projection=pd.DataFrame(shape[:,xy]).drop_duplicates().values
+projection=pd.DataFrame(shape[:,xz]).drop_duplicates().values
 hull=ConvexHull(projection)
 circle=make_circle(hull.points[hull.vertices])
 
@@ -31,5 +43,14 @@ pltcircle=plt.Circle(circle[0:2],circle[2],alpha=0.2,color='g')
 ax.add_artist(pltcircle)
 plt.axis('equal')
 
-d=attributes['grid_res']
+area=projection.shape[0]*d**2.0
+diam=d*circle[2]*0.5
+#diam=attributes['max_diam']
 area_ratio=projection.shape[0]*d**2.0/(np.pi*(d*circle[2])**2.0)
+mass=shape.shape[0]*d**3.0*916.0
+from heymsfield_2010 import dia2vel
+
+v=dia2vel(diam, 1.0, 1.6e-5, mass, area)
+print(v)
+print(dia2vel(dmax, 1.0, 1.6e-5, mass, area))
+print(dia2vel(attributes['max_diam'], 1.0, 1.6e-5, mass, area))
--- heymsfield_2010.py
+++ heymsfield_2010.py
@@ -4,73 +4,55 @@ Created on Fri May 18 16:20:35 2018
 
 @author: dori
 """
+import numpy as np
 
-subroutine dia2vel_heymsfield10_particles &
-    (errorstatus,&     	! out
-    nDia,& 		!in
-    diaSpec_SI,&	!in
-    rho_air_SI,&	!in
-    nu_SI,&		!in
-    mass,&              !in
-    area,&
-    k,&                 !in
-    velSpec)		!out
+def dia2vel(diaSpec_SI, rho_air_SI, nu_SI, mass, area, k=0.5):
+#   !in
+#      !nDia: no of diameters
+#      !diaSpec_SI = diameter spectrum [m]
+#      !rho_air_SI = density of air [kg/m³]
+#      !nu_SI = kinematic viscosity of air [m²/s]
+#      !mass = mass of the particle [SI]
+#      !area = cross section area [SI]
+#      !out
+#      !velSpec: velocity spectrum [m/s]
+#
+#      !Heymsfield, A. J. & Westbrook, C. D. Advances in the Estimation of Ice Particle Fall Speeds
+#      !Using Laboratory and Field Measurements. Journal of the Atmospheric Sciences 67, 2469–2482 (2010).
+#
+#      ! equations 9-11
+#
+#
+#      ! Modules used:
+#      use settings, only: verbose
+#      use kinds
+#      use constants
+#      use report_module
+#      implicit none
+#      
+#      integer, intent(in) :: nDia
+#      real(kind=dbl), intent(in), dimension(ndia)::diaSpec_SI, mass,area
+#      real(kind=dbl), intent(in) :: rho_air_SI, nu_SI, k
+#      real(kind=dbl), dimension(ndia), intent(out) :: velSpec
+#      real(kind=dbl), dimension(ndia)::area_proj, Xstar, Re
+#      real(kind=dbl) :: delta_0, C_0, eta
+#
+#
+#      integer(kind=long), intent(out) :: errorstatus
+#      integer(kind=long) :: err = 0
+#      character(len=33) :: nameOfRoutine = 'dia2vel_heymsfield10_particles'
 
-      !in
-      !nDia: no of diameters
-      !diaSpec_SI = diameter spectrum [m]
-      !rho_air_SI = density of air [kg/m³]
-      !nu_SI = kinematic viscosity of air [m²/s]
-      !mass = mass of the particle [SI]
-      !area = cross section area [SI]
-      !out
-      !velSpec: velocity spectrum [m/s]
+# no check for baundaries due to different possible particle types
+#     k = 0.5   #defined in the paper
+    delta_0 = 8.0
+    C_0 = 0.35
+    g = 9.81
+    
+    area_proj = area/((np.pi/4.)*diaSpec_SI**2)
+    eta = nu_SI * rho_air_SI #!now dynamic viscosity
 
-      !Heymsfield, A. J. & Westbrook, C. D. Advances in the Estimation of Ice Particle Fall Speeds
-      !Using Laboratory and Field Measurements. Journal of the Atmospheric Sciences 67, 2469–2482 (2010).
-
-      ! equations 9-11
-
-
-      ! Modules used:
-      use settings, only: verbose
-      use kinds
-      use constants
-      use report_module
-      implicit none
-
-      integer, intent(in) :: nDia
-      real(kind=dbl), intent(in), dimension(ndia)::diaSpec_SI, mass,area
-      real(kind=dbl), intent(in) :: rho_air_SI, nu_SI, k
-      real(kind=dbl), dimension(ndia), intent(out) :: velSpec
-      real(kind=dbl), dimension(ndia)::area_proj, Xstar, Re
-      real(kind=dbl) :: delta_0, C_0, eta
-
-
-      integer(kind=long), intent(out) :: errorstatus
-      integer(kind=long) :: err = 0
-      character(len=33) :: nameOfRoutine = 'dia2vel_heymsfield10_particles'
-
-      if (verbose >= 2) call report(info,'Start of ', nameOfRoutine)
-
-      ! no check for baundaries due to different possible particle types
-      err = success
-
-!       k = 0.5d0 !defined in the paper
-      delta_0 = 8.0d0
-      C_0 = 0.35d0
-
-      area_proj = area/((pi/4.d0)*diaSpec_SI**2)
-      eta = nu_SI * rho_air_SI !now dynamic viscosity
-
-      Xstar = 8.d0 * rho_air_SI * mass * g / (pi * area_proj**(1.d0-k) * eta**2) !eq 9
-      Re = delta_0**2/4.d0 * ((1.d0+((4.d0*sqrt(Xstar))/(delta_0**2*sqrt(C_0))))**0.5 - 1 )**2 !eq10
+    Xstar = 8.0*rho_air_SI*mass*g/(np.pi*area_proj**(1.0-k)*eta**2)# !eq 9
+    Re=0.25*delta_0**2*((1.0+((4.0*Xstar**0.5)/(delta_0**2.0*C_0**0.5)))**0.5 - 1 )**2 #!eq10
      
-      velSpec = eta * Re /(rho_air_SI*diaSpec_SI)
-
-      errorstatus = err
-      if (verbose >= 2) call report(info,'End of ', nameOfRoutine)
-
-      return
-
-  end subroutine dia2vel_heymsfield10_particles
+    velSpec = eta*Re/(rho_air_SI*diaSpec_SI)
+    return velSpec
